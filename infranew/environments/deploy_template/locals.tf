8f2a82eaeec8bbc947ba73b0f3d04b94e2b86426
Mohammad Irfan <irfanmd40351@gmail.com>
2025-11-14 18:00:51 +0530

    Fix: Shorten VM name and re-apply networking lookup fix

--- infranew/environments/deploy_template/locals.tf
+++ infranew/environments/deploy_template/locals.tf
@@ -14,7 +14,10 @@ locals {
   # 3. Resource Names
   # Example: "Voltica-tst-vnet"
   vnet_name                  = "${local._name_prefix}-vnet"
-  vm_name                    = "${local._name_prefix}-sql-vm"
+  
+  # --- FIX: Shortened name from 'sql-vm' (16 chars) to 'sqlvm' (15 chars) ---
+  vm_name                    = "${local._name_prefix}-sqlvm" 
+  
   storage_account_name       = "st${replace(lower(var.client_name), "-", "")}${var.environment_name}771" # Must be globally unique
   app_service_plan_name      = "${local._name_prefix}-asp"
   service_bus_namespace_name = "${local._name_prefix}-sb-namespace"
@@ -28,7 +31,6 @@ locals {
   subnets = {
     "vm-subnet" = {
       address_prefixes  = ["10.0.1.0/24"]
-      # --- THIS IS THE FIX ---
       service_endpoints = [] 
     }
     (local.private_endpoints_subnet_name) = {
@@ -43,5 +45,4 @@ resource "random_string" "kv_suffix" {
   length  = 5
   special = false
   upper   = false
-}# Force cache bust v4
-# Force cache bust v4
+}
