7d3a84c9ace3838a02d648ed6395ee3696a0b82b
Mohammad Irfan <irfanmd40351@gmail.com>
2025-11-16 23:11:27 +0530

    Fix: Update Key Vault module to accept and create secrets

--- infranew/modules/key_vault/main.tf
+++ infranew/modules/key_vault/main.tf
@@ -1,9 +1,67 @@
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_key_vault" "kv" {
   name                        = var.key_vault_name
   location                    = var.location
   resource_group_name         = var.resource_group_name
+  enabled_for_disk_encryption = true
   tenant_id                   = var.tenant_id
-  sku_name                    = "standard"
-  enable_rbac_authorization   = true # Using RBAC for access
-  tags                        = var.tags
+  soft_delete_retention_days  = 90
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  # Access policy for the Service Principal running Terraform
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get", "Create", "Delete", "List", "Update", "Import", "Backup", "Restore", "Recover"
+    ]
+
+    secret_permissions = [
+      "Get", "List", "Set", "Delete", "Backup", "Restore", "Recover", "Purge"
+    ]
+
+    certificate_permissions = [
+      "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore", "ManageContacts", "ManageIssuers", "GetIssuers", "ListIssuers", "SetIssuers", "DeleteIssuers"
+    ]
+  }
+  
+  # Enable RBAC (Role Based Access Control)
+  enable_rbac_authorization = true
+
+  tags = var.tags
+}
+
+# --- CREATE THE SECRETS ---
+
+resource "azurerm_key_vault_secret" "auth0_domain" {
+  name         = "Auth0-Domain"
+  value        = var.auth0_domain
+  key_vault_id = azurerm_key_vault.kv.id
+  depends_on   = [azurerm_key_vault.kv]
+}
+
+resource "azurerm_key_vault_secret" "mailgun_key" {
+  name         = "Mailgun-ApiKey"
+  value        = var.mailgun_key
+  key_vault_id = azurerm_key_vault.kv.id
+  depends_on   = [azurerm_key_vault.kv]
+}
+
+resource "azurerm_key_vault_secret" "twilio_sid" {
+  name         = "Twilio-SID"
+  value        = var.twilio_sid
+  key_vault_id = azurerm_key_vault.kv.id
+  depends_on   = [azurerm_key_vault.kv]
+}
+
+output "id" {
+  value = azurerm_key_vault.kv.id
+}
+
+output "name" {
+  value = azurerm_key_vault.kv.name
 }
