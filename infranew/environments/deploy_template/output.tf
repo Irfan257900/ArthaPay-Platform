96a5613cdf5caf869207242163c787617edb6bae
Mohammad Irfan <irfanmd40351@gmail.com>
2025-11-20 16:12:59 +0530

    Feat : new

--- infranew/environments/deploy_template/output.tf
+++ infranew/environments/deploy_template/output.tf
@@ -7,26 +7,26 @@ output "vm_rg_name" {
   value = azurerm_resource_group.rg_infra.name
 }
 
-# --- Web App Outputs ---
+# --- Web App (Container) Outputs (FROM MODULE) ---
 output "webapp_name" {
-  value = azurerm_linux_web_app.ui_webapp.name
+  value = module.container_app.webapp_name
 }
 
 output "webapp_rg_name" {
   value = azurerm_resource_group.rg_apps.name
 }
 
-# --- ACR Outputs (LOWERCASE KEYS) ---
+# --- ACR Outputs (FROM MODULE) ---
 output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
+  value = module.container_app.acr_login_server
 }
 
 output "acr_admin_username" {
-  value = azurerm_container_registry.acr.admin_username
+  value = module.container_app.acr_admin_username
   sensitive = true
 }
 
 output "acr_admin_password" {
-  value = azurerm_container_registry.acr.admin_password
+  value = module.container_app.acr_admin_password
   sensitive = true
 }
