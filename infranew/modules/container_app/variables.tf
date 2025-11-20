07f4ebb49ebcf7d64c6cec4f1bd317467960865c
Mohammad Irfan <irfanmd40351@gmail.com>
2025-11-20 18:28:44 +0530

    Feat : new

--- infranew/modules/container_app/variables.tf
+++ infranew/modules/container_app/variables.tf
@@ -5,3 +5,4 @@ variable "tags" { type = map(string) }
 variable "acr_name" { type = string }
 variable "app_service_plan_name" { type = string }
 variable "web_app_name" { type = string }
+variable "webapp_rg_name" { type = string }
