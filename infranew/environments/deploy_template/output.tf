e80c21f3ede08cbcd06576600c43d85e01945be0
Mohammad Irfan <irfanmd40351@gmail.com>
2025-11-20 11:49:32 +0530

    Fix: Update outputs to match Container App architecture

--- infranew/environments/deploy_template/output.tf
+++ infranew/environments/deploy_template/output.tf
@@ -1,23 +1,45 @@
-# This file exposes outputs from our modules to the root,
-# so the GitHub workflow can read them.
-
-output "static_web_app_api_key" {
-  description = "The deployment token for the Static Web App"
-  value       = module.static_web_app.api_key
-  sensitive   = true
-}
-
-output "static_web_app_url" {
-  description = "The URL of the Static Web App"
-  value       = module.static_web_app.url
-}
-
+# --- Virtual Machine Outputs ---
 output "vm_name" {
-  description = "The name of the SQL VM"
-  value       = local.vm_name
+  description = "The name of the SQL Virtual Machine"
+  value       = azurerm_windows_virtual_machine.vm.name
 }
 
 output "vm_rg_name" {
-  description = "The name of the VM's resource group"
-  value       = local.vm_rg_name
+  description = "The resource group of the VM"
+  value       = azurerm_resource_group.rg_infra.name
+}
+
+# --- Web App (Container) Outputs ---
+output "webapp_name" {
+  description = "The name of the Linux Web App for the UI"
+  value       = azurerm_linux_web_app.ui_webapp.name
+}
+
+output "webapp_rg_name" {
+  description = "The resource group of the Web App"
+  value       = azurerm_resource_group.rg_apps.name
+}
+
+# --- ACR Outputs ---
+output "acr_login_server" {
+  description = "The login server URL for the Container Registry"
+  value       = azurerm_container_registry.acr.login_server
+}
+
+output "acr_admin_username" {
+  description = "ACR Admin Username"
+  value       = azurerm_container_registry.acr.admin_username
+  sensitive   = true
+}
+
+output "acr_admin_password" {
+  description = "ACR Admin Password"
+  value       = azurerm_container_registry.acr.admin_password
+  sensitive   = true
+}
+
+# --- Function App Outputs (Optional/Debug) ---
+output "function_app_names" {
+  description = "Names of the deployed backend Function Apps"
+  value       = [for f in module.function_apps : f.function_app_name]
 }
