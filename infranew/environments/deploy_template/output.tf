ad80d5ce4642efc86cbe2e64bbe830a6f75f2aba
Mohammad Irfan <irfanmd40351@gmail.com>
2025-11-20 15:19:04 +0530

    Fix: Enforce lowercase outputs across all workflows

--- infranew/environments/deploy_template/output.tf
+++ infranew/environments/deploy_template/output.tf
@@ -1,26 +1,19 @@
-# --- Virtual Machine Outputs ---
 output "vm_name" {
-  description = "The name of the SQL Virtual Machine"
-  value       = azurerm_windows_virtual_machine.vm.name
+  value = azurerm_windows_virtual_machine.vm.name
 }
 
 output "vm_rg_name" {
-  description = "The resource group of the VM"
-  value       = azurerm_resource_group.rg_infra.name
+  value = azurerm_resource_group.rg_infra.name
 }
 
-# --- Web App (Container) Outputs ---
 output "webapp_name" {
-  description = "The name of the Linux Web App for the UI"
-  value       = azurerm_linux_web_app.ui_webapp.name
+  value = azurerm_linux_web_app.ui_webapp.name
 }
 
 output "webapp_rg_name" {
-  description = "The resource group of the Web App"
-  value       = azurerm_resource_group.rg_apps.name
+  value = azurerm_resource_group.rg_apps.name
 }
 
-# --- ACR Outputs (Make sure these are here!) ---
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
