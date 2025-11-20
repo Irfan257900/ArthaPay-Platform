9f1783f7c0095617aab848d00d20776983a056f2
Mohammad Irfan <irfanmd40351@gmail.com>
2025-11-20 18:18:21 +0530

    Feat : add

--- infranew/modules/container_app/output.tf
+++ infranew/modules/container_app/output.tf
@@ -1,16 +1,16 @@
 output "webapp_name" {
   value = azurerm_linux_web_app.ui_webapp.name
 }
-
+output "webapp_rg_name" {
+  value = azurerm_resource_group.rg_apps.name
+}
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
-
 output "acr_admin_username" {
   value = azurerm_container_registry.acr.admin_username
   sensitive = true
 }
-
 output "acr_admin_password" {
   value = azurerm_container_registry.acr.admin_password
   sensitive = true
