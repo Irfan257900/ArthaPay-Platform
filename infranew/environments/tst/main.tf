ac1802c6b0ece1572323741a83324ef8a2602194
Mohammad Irfan <irfanmd40351@gmail.com>
2025-11-25 17:00:54 +0530

    Feat add new

--- infranew/environments/tst/main.tf
+++ infranew/environments/tst/main.tf
@@ -115,6 +115,46 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+# --- NETWORK SECURITY GROUP (NSG) ---
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${local._name_prefix}-nsg"
+  location            = azurerm_resource_group.rg_infra.location
+  resource_group_name = azurerm_resource_group.rg_infra.name
+  tags                = local.common_tags
+
+  # Rule 1: Allow SQL (1433)
+  security_rule {
+    name                       = "AllowSQL"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "1433"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Rule 2: Allow RDP (3389)
+  security_rule {
+    name                       = "AllowRDP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# --- ATTACH NSG TO NIC ---
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
 # --- SQL Virtual Machine ---
 resource "azurerm_windows_virtual_machine" "vm" {
   name                = local.vm_name
