087d9f888efb17c083ceb9540c0ea396836a0b33
Mohammad Irfan <irfanmd40351@gmail.com>
2025-12-03 14:37:16 +0530

    add new

--- infranew/modules/service_bus/main.tf
+++ infranew/modules/service_bus/main.tf
@@ -1,11 +1,4 @@
-resource "azurerm_servicebus_namespace" "sb_namespace" {
-  name                = var.service_bus_namespace_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  sku                 = var.sku
-  tags                = var.tags
-}
-
+# --- SERVICE BUS NAMESPACE (Create this ONLY ONCE) ---
 resource "azurerm_servicebus_namespace" "sb_namespace" {
   name                = var.service_bus_namespace_name
   location            = var.location
